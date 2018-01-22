4a7f4d370e70372c42ea992419181808396d7b06
Morton358 <vovankovalchuk358@gmail.com>
2018-01-22 19:37:52 +0100

    edit some style indentation

--- app/mother-child.js
+++ app/mother-child.js
@@ -1,39 +1,40 @@
-require(['ancestry'], function (ancestry) {
-  var data = JSON.parse(ANCESTRY_FILE);
+require(['ancestry'], function(ancestry) {
+    var data = JSON.parse(ANCESTRY_FILE);
 
-  function average(array) {
-    function plus(a, b) { return a + b; }
-    return array.reduce(plus) / array.length;
-  }
+    function average(array) {
+        function plus(a, b) {
+            return a + b;
+        }
+        return array.reduce(plus) / array.length;
+    }
 
-  var byName = {};
-  data.forEach(function(person) {
-    byName[person.name] = person.born;
-  });
+    var byName = {};
+    data.forEach(function(person) {
+        byName[person.name] = person.born;
+    });
 
-  function bouncer(arr) {
-  return arr.filter(Boolean);
-  }
+    function bouncer(arr) {
+        return arr.filter(Boolean);
+    }
 
-  //function withMam(person) { return byName[person.mother] != 'undefined'; }
+    //function withMam(person) { return byName[person.mother] != 'undefined'; }
 
-  /*function hasKnownMother(p) {
+    /*function hasKnownMother(p) {
     data.some(function(el) {
       return el["name"] === p["mother"];
     });
   }*/
 
-  function motherChild() {
-    var diff = [];
-    data.forEach(function(person) {
-      diff.push(person.born - byName[person.mother]);
-    });
-    res = bouncer(diff);
-    return res;
-  }
+    function motherChild() {
+        var diff = [];
+        data.forEach(function(person) {
+            diff.push(person.born - byName[person.mother]);
+        });
+        res = bouncer(diff);
+        return res;
+    }
 
-  console.log(average(motherChild()));
-
+    console.log(average(motherChild()));
 });
 
 // → 31.2
